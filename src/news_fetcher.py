512dac585bbe6adb5d157cd7eee114531bbd883e
Martin Mackovík <martasekm@gmail.com>
2022-11-19 11:01:48 +0100

    add news_fetcher.py file for fetching the news

---
+++ src/news_fetcher.py
@@ -0,0 +1,18 @@
+def _get_news_single_rss(rss_feed: str):
+    pass
+
+
+def get_news_all_rss(user_id: int):
+    pass
+
+
+def get_rss_feed_list(user_id: int):
+    pass
+
+
+def add_rss_feed(user_id: int, rss_feed: str):
+    pass
+
+
+def rm_rss_feed(user_id: int, rss_feed_id: int):
+    pass
