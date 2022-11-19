56c6c0fb9d62bbb9ed08a27aab67f74cfe9eb773
Martin Mackovík <martasekm@gmail.com>
2022-11-19 11:03:13 +0100

    create NewsFetcher class inside news_fetcher.py

--- src/news_fetcher.py
+++ src/news_fetcher.py
@@ -1,18 +1,21 @@
-def _get_news_single_rss(rss_feed: str):
-    pass
+import feedparser
 
 
-def get_news_all_rss(user_id: int):
-    pass
+class NewsFetcher:
+    def __init__(self):
+        self.tmp_feed_list = dict()
 
+    def _get_news_single_rss(self, rss_feed: str):
+        pass
 
-def get_rss_feed_list(user_id: int):
-    pass
+    def get_news_all_rss(self, user_id: int):
+        pass
 
+    def get_rss_feed_list(self, user_id: int):
+        pass
 
-def add_rss_feed(user_id: int, rss_feed: str):
-    pass
+    def add_rss_feed(self, user_id: int, rss_feed: str):
+        pass
 
-
-def rm_rss_feed(user_id: int, rss_feed_id: int):
-    pass
+    def rm_rss_feed(self, user_id: int, rss_feed_id: int):
+        pass
