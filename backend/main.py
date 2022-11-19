dd3529811b0a6c036be2111f9c459a27e8aae503
Martin Mackovík <martasekm@gmail.com>
2022-11-19 09:40:19 +0100

    add default Python boilerplate into the main.py file

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main(args):
+    """ Main entry point of the app """
+    print("hello world")
+    print(args)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Required positional argument
+    parser.add_argument("arg", help="Required positional argument")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--name", action="store", dest="name")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
