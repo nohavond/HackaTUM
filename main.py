8b34409233cbe98296268f4dcdf529f58a5eeed3
Martin Mackovík <martasekm@gmail.com>
2022-11-20 01:29:51 +0100

    rename api_endpoint dir to api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import api_endpoint.api_endpoint as api
-import api_endpoint.Database as db
+import api.api_endpoint as api
+import api.Database as db
 
 if __name__ == '__main__':
     database = db.CDatabase()  # initialize create the database
