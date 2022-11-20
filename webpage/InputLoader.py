dfaf32689f23f6be6dd9d57d27c73e36d5fa4da1
Martin Mackovík <martasekm@gmail.com>
2022-11-20 01:22:10 +0100

    add absolut path to db in InputLoader.py

--- webpage/InputLoader.py
+++ webpage/InputLoader.py
@@ -2,5 +2,5 @@ from Database import CDatabase
 
 
 def load_users():
-    db = CDatabase()
+    db = CDatabase(r"C:\Users\Martin\Desktop\TUM\HackaTUM\api_endpoint\users.db")
     return db.get_data()
